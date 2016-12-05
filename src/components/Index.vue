60c3cb55426652d437b7fa23edc29f4e535d7050
Taylor Hessel <taylorhessel@gmail.com>
2016-12-05 11:46:17 -0700

    Finished large device home page design

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,8 +2,14 @@
   <div class="index">
     <img src="../assets/images/index-bg.jpg" class="bg" alt="tRUST">
     <div class="overlay"></div>
-    <div class="index-content ink-grid">
-      <h2>Frontend is hard</h2>
+    <div class="index-content ink-grid clearfix">
+      <div class="column-group marketing push-right">
+        <h1 class="align-center">A Rust social networking resource</h1>
+        <h2 class="align-right">Login and group up today</h2>
+        <div class="button-wrapper align-right">
+          <button class="ink-button" type="button" name="login">login with Steam</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,12 +1,9 @@
 <template>
   <footer class="clearfix">
       <div class="ink-grid">
-          <ul class="unstyled inline half-vertical-space">
-              <li class="active"><a href="#">About</a></li>
-              <li><a href="#">Sitemap</a></li>
-              <li><a href="#">Contacts</a></li>
-          </ul>
-          <p class="note">Identification of the owner of the copyright, either by name, abbreviation, or other designation by which it is generally known.</p>
+        <div class="column-group">
+          <p class="note">&copy; 2016 by Taylor Hessel</p>
+        </div>
       </div>
   </footer>
 </template>
